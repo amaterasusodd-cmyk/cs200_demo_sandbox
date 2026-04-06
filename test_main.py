0163eea2713d03fc3baddb54c9c7977c6f1c634d
amaterasusodd-cmyk <amaterasusodd@gmail.com>
2026-04-06 11:06:51 -0400

    linting

--- test_main.py
+++ test_main.py
@@ -18,13 +18,14 @@ def test_foo(input_x, input_y, expected):
     # assert main.foo(1, 1) == 1
     assert main.foo(input_x, input_y) == expected
 
+
 @pytest.mark.parametrize(
-    ('input_x','expected'),
+    ('input_x', 'expected'),
     (
-        (0,0),
-        (1,5),
-        (2,10),
-        (10,50)
+        (0, 0),
+        (1, 5),
+        (2, 10),
+        (10, 50)
     )
 )
 def test_bar(input_x, expected):
